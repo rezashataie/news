d84a4c260ffc5292599075a02f770b8eb214531e
rezashataie <rezashataie75@gmail.com>
2023-11-04 17:15:30 +0330

    complete add category in admin

--- activities/Admin/Category.php
+++ activities/Admin/Category.php
@@ -13,4 +13,16 @@ class Category extends Admin
         $categories = $db->select('SELECT * FROM categories ORDER BY `id` DESC');
         require_once(BASE_PATH . '/template/admin/categories/index.php');
     }
+
+    public function create()
+    {
+        require_once(BASE_PATH . '/template/admin/categories/create.php');
+    }
+
+    public function store($request)
+    {
+        $db = new DataBase();
+        $db->insert('categories', array_keys($request), $request);
+        $this->redirect('admin/category');
+    }
 }
--- template/admin/categories/index.php
+++ template/admin/categories/index.php
@@ -4,8 +4,7 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h5"><i class="fas fa-newspaper"></i> Categories</h1>
     <div class="btn-toolbar mb-2 mb-md-0">
-        <a role="button" href="" class="btn btn-sm btn-success">create
-        </a>
+        <a role="button" href="<?= url('admin/category/create') ?>" class="btn btn-sm btn-success">create</a>
     </div>
 </div>
 <div class="table-responsive">
