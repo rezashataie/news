6853cf9ab726cfc613a2d0718aa9c8d172cb4752
rezashataie <rezashataie75@gmail.com>
2023-11-05 15:11:33 +0330

    add jalali date

--- template/admin/layouts/header.php
+++ template/admin/layouts/header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.6/css/mdb.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= asset('public/jalalidatepicker/persian-datepicker.min.css') ?>" type="text/css">
     <link href="<?= asset('public/admin-panel/css/style.css') ?>" rel="stylesheet" type="text/css">
 </head>
 
