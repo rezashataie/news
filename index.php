fab5b144003d03f12d570ee90cf02376abf06bf1
rezashataie <rezashataie75@gmail.com>
2023-11-05 10:41:38 +0330

    add routing rules for posts section

--- index.php
+++ index.php
@@ -137,4 +137,12 @@ uri('admin/category/edit/{id}', 'Admin\Category', 'edit');
 uri('admin/category/update/{id}', 'Admin\Category', 'update', 'POST');
 uri('admin/category/delete/{id}', 'Admin\Category', 'delete');
 
+// post
+uri('admin/post', 'Admin\Post', 'index');
+uri('admin/post/create', 'Admin\Post', 'create');
+uri('admin/post/store', 'Admin\Post', 'store', 'POST');
+uri('admin/post/edit/{id}', 'Admin\Post', 'edit');
+uri('admin/post/update/{id}', 'Admin\Post', 'update', 'POST');
+uri('admin/post/delete/{id}', 'Admin\Post', 'delete');
+
 echo '404 - page not found';
